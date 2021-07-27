c25b6ed87a52bdaf2ea473965d36b4d527f01784
Lesedi <leseditumelo32@gmail.com>
2021-07-27 19:49:28 +0200

    creating tables

--- try_it_yourself/try_it_yourself.sql
+++ try_it_yourself/try_it_yourself.sql
@@ -97,7 +97,14 @@ CREATE TABLE albums (
 	album_genre varchar(40),
 	album_description text
 );
-
+INSERT INTO teachers (first_name, last_name, school, hire_date, salary)
+VALUES 	('Janet', 'Smith', 'F.D. Roosevelt HS', '2011-10-30', 36200),
+		('Lee', 'Reynolds', 'F.D. Roosevelt HS', '1993-05-22', 65000),
+		('Samuel', 'Cole', 'Myers Middle School', '2005-08-01', 43500),
+		('Samantha', 'Bush', 'Myers Middle School', '2011-10-30', 36200),
+		('Betty', 'Diaz', 'Myers Middle School', '2005-08-30', 43500),
+		('Kathleen', 'Roush', 'F.D. Roosevelt HS', '2010-10-22', 38500);
+		
 CREATE TABLE songs (
 	song_id bigserial,
 	song_title text,
@@ -105,3 +112,6 @@ CREATE TABLE songs (
 	album_id bigint
 );
 
+------
+
+		
