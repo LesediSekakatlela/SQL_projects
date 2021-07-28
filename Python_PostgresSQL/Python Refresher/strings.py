0a57011404b9f9cc80cc0c5b324990b65c30c092
Lesedi <leseditumelo32@gmail.com>
2021-07-28 17:55:46 +0200

    using with

--- Python_PostgresSQL/Python Refresher/strings.py
+++ Python_PostgresSQL/Python Refresher/strings.py
@@ -5,3 +5,9 @@ with_name_two = greeting.format("Rolf")
 
 print(with_name)
 print(with_name_two)
+
+
+longer_phrase = "Hello, {}. Today is {}."
+
+formatted = longer_phrase.format("Rolf","Monday")
+print(formatted)
