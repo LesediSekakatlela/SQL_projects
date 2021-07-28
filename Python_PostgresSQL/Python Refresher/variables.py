f55d14d8cba5baed4f3790ae62d1c268663ea756
Lesedi <leseditumelo32@gmail.com>
2021-07-28 16:31:20 +0200

    variables

--- Python_PostgresSQL/Python Refresher/variables.py
+++ Python_PostgresSQL/Python Refresher/variables.py
@@ -4,5 +4,21 @@ price = 9.99
 discount = 0.2
 
 result = price * (1 - discount)
-
 print(result)
+
+
+name = "Rolf"
+print(name)
+#print(name * 2)--joins 2 strings
+
+
+a = 25
+b = a
+
+print(a)
+print(b)
+
+b = 17
+
+print(a)
+print(b)
