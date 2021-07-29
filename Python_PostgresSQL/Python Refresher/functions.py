4a78767567f28f27c5cbfdd761315660ab45cb1d
Lesedi <leseditumelo32@gmail.com>
2021-07-29 19:04:39 +0200

    using seconds

--- Python_PostgresSQL/Python Refresher/functions.py
+++ Python_PostgresSQL/Python Refresher/functions.py
@@ -1,4 +1,9 @@
-def hello():
-    print("Hello!")
+def user_age_in_seconds():
+    user_age = int(input("Enter your age: "))
+    age_seconds = user_age * 365 * 24 * 60 * 60
+    print(f"Your age in seconds is {age_seconds}.")
 
-hello()
+print("Welcome to the age in seconds program!")
+user_age_in_seconds()
+
+print("Goodbye!")
