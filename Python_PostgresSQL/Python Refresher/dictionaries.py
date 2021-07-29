97d37e0ef34bf32abe21ea9869ee21de1f3a60f1
Lesedi <leseditumelo32@gmail.com>
2021-07-29 18:19:01 +0200

    friend dictionary

--- Python_PostgresSQL/Python Refresher/dictionaries.py
+++ Python_PostgresSQL/Python Refresher/dictionaries.py
@@ -1,5 +1,15 @@
 friend_ages = {"Rolf": 24, "Adam": 30, "Anne": 27}
 
 friend_ages["Rolf"] = 20
-
 print(friend_ages)
+
+#2
+friends = [
+    {"name": "Rolf", "age": 24},
+    {"name": "Adam", "age": 30},
+    {"name": "Anne", "age": 27},
+]
+
+print(friends)
+
+#3
