8308934e6cf508ccfbc3ab6d85226f782ca4d2ee
Lesedi <leseditumelo32@gmail.com>
2021-07-29 10:30:23 +0200

    advanced_set_operations

--- Python_PostgresSQL/Python Refresher/advanced_set_operations.py
+++ Python_PostgresSQL/Python Refresher/advanced_set_operations.py
@@ -1,3 +1,17 @@
+friends = {"Bob","Rolf","Anne"}
+abroad = {"Bob","Anne"}
+
+local_friends = friends.difference(abroad)
+print(local_friends)
+
+
+local = {"Rolf"}
+abroad = {"Bob","Anne"}
+
+friends = local.union(abroad)
+print(friends)
+
+
 art = {"Bob","Jen","Rolf","Chris"}
 science = {"Bob","Jen","Paul","Anne"}
 
