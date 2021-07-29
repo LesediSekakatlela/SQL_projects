1bd07a6ef9595fcf4c908bf292d5d01beb4d030d
Lesedi <leseditumelo32@gmail.com>
2021-07-29 18:07:44 +0200

    list_comprehension

--- Python_PostgresSQL/Python Refresher/list_comprehension.py
+++ Python_PostgresSQL/Python Refresher/list_comprehension.py
@@ -1,8 +1,7 @@
 friends = ["Rolf", "Sam","Samantha", "Saurabh" ,"Jen"]
-starts_s = []
+starts_s = [friend for friend in friends if friend.startswith("S")]
 
-for friend in friends:
-    if friend.startswith("S"):
-        starts_s.append(friend)
-
+print(friends)
 print(starts_s)
+print(friends is starts_s)
+print("friends: ", id(friends), "starts_s", id(starts_s))
