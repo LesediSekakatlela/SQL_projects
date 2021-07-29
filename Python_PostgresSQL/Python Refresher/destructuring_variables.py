8c3cc798a65e1b5c28f10b7a398e7061dac50d0a
Lesedi <leseditumelo32@gmail.com>
2021-07-29 18:48:07 +0200

    profession

--- Python_PostgresSQL/Python Refresher/destructuring_variables.py
+++ Python_PostgresSQL/Python Refresher/destructuring_variables.py
@@ -7,3 +7,12 @@ print(x, y)
 students_attendance = {"Rolf": 96, "Bob": 80, "Anne": 100}
 
 print(list(students_attendance.items()))
+
+for t in students_attendance.items():
+    print(t)
+
+#3
+people = [("Bob", 42, "Mechanic"), ("James", 24, "Artist"), ("Harry", 32, "Lecturer")]
+
+for name, age, profession in people:
+    print(f"Name: {name}, Age: {age}, Profession: {profession}")
