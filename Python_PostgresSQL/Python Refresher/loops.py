a8905adc987c2d69250c28eb601669808d950f93
Lesedi <leseditumelo32@gmail.com>
2021-07-29 17:27:39 +0200

    python for loops

--- Python_PostgresSQL/Python Refresher/loops.py
+++ Python_PostgresSQL/Python Refresher/loops.py
@@ -1,15 +1,4 @@
-number = 7
+friends = ["Rolf", "Jen", "Bob", "Anne"]
 
-while True:
-    user_input = input("Would you like to play? (Y/n) ")
-
-    if user_input == "n":
-        break
-
-    user_number = int(input("Guess our number: "))
-    if user_number == number:
-        print("You guessed correctly!")
-    elif abs(number - user_number) == 1:
-        print("You were off by one.")
-    else:
-        print("Sorry, it's wrong!")
+for friend in range(4):
+     print(f"{friend} is my friend.")
