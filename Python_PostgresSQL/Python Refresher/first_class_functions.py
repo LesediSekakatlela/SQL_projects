db3258503b9c770bbaab6a5d152a5dab6e5229ec
Lesedi <leseditumelo32@gmail.com>
2021-08-01 17:55:48 +0200

    first_class_functions

--- Python_PostgresSQL/Python Refresher/first_class_functions.py
+++ Python_PostgresSQL/Python Refresher/first_class_functions.py
@@ -9,3 +9,20 @@ def calculate(*values, operator):
 
 result = calculate(20, 4, operator=divide)
 print(result)
+
+
+from operator import itemgetter
+
+def search(sequence, expected, finder):
+    for elem in sequence:
+        if finder(elem) == expected:
+            return elem
+    raise RuntimeError(f"Could not find an element with {expected}.")
+
+friends = [
+    {"name": "Rolf Smith", "age": 24},
+    {"name": "Adam Wool", "age": 30},
+    {"name": "Anne Pun", "age": 27},
+]
+
+print(search(friends, "Rolf Smith", itemgetter("name")))
