0df37306a504f3214a6ee1190bc8f09b2a47e840
Lesedi <leseditumelo32@gmail.com>
2021-08-01 17:03:34 +0200

    custom_error_classes

--- Python_PostgresSQL/Python Refresher/custom_error_classes.py
+++ Python_PostgresSQL/Python Refresher/custom_error_classes.py
@@ -1,3 +1,6 @@
+class TooManyPagesReadError(ValueError):
+    pass
+
 class Book:
     def __init__(self, name: str, page_count: int):
         self.name = name
@@ -10,9 +13,18 @@ class Book:
         )
     
     def read(self, pages: int):
+        if self.pages_read + pages > self.page_count:
+            raise TooManyPagesReadError(
+                f"You tried to read {self.pages_read + pages} pages, but this book only has {self.page_count} pages."
+            )
+
         self.pages_read += pages
         print(f"You have now read {self.pages_read} pages out of {self.page_count}.")
 
 python101 = Book("Python 101", 50)
-python101.read(35)
-python101.read(50)
+
+try:
+    python101.read(35)
+    python101.read(50)
+except TooManyPagesReadError as e:
+    print(e)
