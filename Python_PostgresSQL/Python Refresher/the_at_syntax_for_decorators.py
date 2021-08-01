96a244a9c2e10e0420a8a3058a10118590d44868
Lesedi <leseditumelo32@gmail.com>
2021-08-01 18:35:43 +0200

    the_at_syntax_for_decorators

--- Python_PostgresSQL/Python Refresher/the_at_syntax_for_decorators.py
+++ Python_PostgresSQL/Python Refresher/the_at_syntax_for_decorators.py
@@ -1,7 +1,10 @@
-user = {"username": "jose", "access_level": "admin"}
+import functools
+
+user = {"username": "jose", "access_level": "guest"}
 
 
 def make_secure(func):
+    @functools.wraps(func)
     def secure_function():
         if user["access_level"] == "admin":
             return func()
@@ -14,4 +17,4 @@ def make_secure(func):
 def get_admin_password():
     return "1234"
 
-print(get_admin_password())
+print(get_admin_password.__name__)
