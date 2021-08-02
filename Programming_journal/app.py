a9ae829fed1c25e8f59d5c7b2fc0427ff30faaea
Lesedi <leseditumelo32@gmail.com>
2021-08-02 16:36:02 +0200

    lists in database

--- Programming_journal/app.py
+++ Programming_journal/app.py
@@ -1,17 +1,32 @@
+from database import add_entry, get_entries
+
 menu = """Please select one of the following options:
 1)Add new entry for today.
 2)View entries.
 3)Exit.
 
 Your selection:"""
-welcom = "Welcom to the programming daiary!"
+welcom = "Welcom to the programming diary!"
+
+
+def prompt_new_entry():
+    entry_content = input("What have you learned today? ")
+    entry_date = input("Enter the date: ")
+
+    add_entry(entry_content, entry_date)
+
+
+def view_entries(entries):
+    for entry in entries:
+            print(f"{entry['date']}\n{entry['content']}\n\n")
+
 
 print(welcom)
 
 while (user_input := input(menu)) != "3":  #We'll deal with user input here..
     if user_input == "1":
-        print("Adding...")
+       prompt_new_entry()
     elif user_input == "2":
-        print("Viewing...") 
+        view_entries(get_entries())
     else:
         print("Invaild option,please try again!")
