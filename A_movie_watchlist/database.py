49f84f1ff958122411ab4befebffdbfe0c13ae38
Lesedi <leseditumelo32@gmail.com>
2021-08-05 12:17:00 +0200

    app with python

--- A_movie_watchlist/database.py
+++ A_movie_watchlist/database.py
@@ -29,6 +29,7 @@ JOIN users ON users.username = watched.user_username
 WHERE users.username = ?;"""
 INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_username, movie_id) VALUES (?, ?)"
 SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?;"
+SEARCH_MOVIES = "SELECT * FROM movies WHERE title LIKE ?;"
 
 
 connection = sqlite3.connect("data.db")
@@ -60,6 +61,11 @@ def get_movies(upcoming=False):
             cursor.execute(SELECT_ALL_MOVIES)
         return cursor.fetchall()
 
+
+def search_movies(search_term):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SEARCH_MOVIES, (f"%{search_term}%",))
 
 def watch_movie(username, movie_id):
     with connection:
