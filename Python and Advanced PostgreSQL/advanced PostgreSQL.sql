8d4b1ba0b6512544512c05693c0fdaa454440715
Lesedi <leseditumelo32@gmail.com>
2021-08-17 17:34:38 +0200

    composite types

--- Python and Advanced PostgreSQL/advanced PostgreSQL.sql
+++ Python and Advanced PostgreSQL/advanced PostgreSQL.sql
@@ -23,4 +23,12 @@ VALUES 	('Bob Smith', 'bob@gmail.com', 65615),
 		( 'Susan Williams', 'swilliams@gmail.com', 56),
 		( 'Anne Pun', 'annepun@gmail.com', 2419200);
 		
+CREATE OR REPLACE FUNCTION delete_inactive(seconds numeric) RETURNS bigint AS $$
+	WITH deleted AS (DELETE FROM users WHERE last_opened > seconds RETURNING *)
+	SELECT COUNT(*) FROM deleted;
+$$ LANGUAGE SQL;
+
+DROP FUNCTION delete_inactive(numeric);
+SELECT delete_inactive(86400);
 SELECT * FROM users;
+
