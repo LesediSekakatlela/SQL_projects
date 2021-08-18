54e0331e2d6d45f472d5030b9408165bfe05baff
Lesedi <leseditumelo32@gmail.com>
2021-08-18 19:19:54 +0200

    axis and plots

--- Charting data/pysql-course-projectst/matplotlib-polls/graph_tests.py
+++ Charting data/pysql-course-projectst/matplotlib-polls/graph_tests.py
@@ -6,5 +6,12 @@ plt.xlabel("Categories")
 plt.ylabel("Amounts")
 plt.title("Categories vs. Amounts")
 
-lines = plt.plot(["Men", "Women", "Children"], [3, 5, 9])
+lines = plt.plot(["Men", "Women", "Children"], [3, 5, 9], "o")
 plt.show()
+
+# import matplotlib.pyplot as plt
+#
+# plt.figure()
+# plt.plot([1, 2, 3, 4], [3, 5, 9, 25])
+# plt.show()
+
