55d5461dbf22038371f8e9eafdae4998031d05cc
Lesedi <leseditumelo32@gmail.com>
2021-08-19 12:21:56 +0200

    draw pie chart

--- Charting data/pysql-course-projectst/matplotlib-polls/graph_tests.py
+++ Charting data/pysql-course-projectst/matplotlib-polls/graph_tests.py
@@ -17,14 +17,34 @@
 # plt.plot([1, 2, 3, 4], [3, 5, 9, 25])
 # plt.show()
 
+# --add multiple subplots--
+# import matplotlib.pyplot as plt
+#
+# figure = plt.figure()
+# ax1 = figure.add_subplot(1, 2, 1)
+# ax2 = figure.add_subplot(1, 2, 2)
+#
+# ax1.plot([1, 2, 3, 4], [3, 5, 9, 25])
+# ax2.plot([1, 2, 3, 4], [5, 7, 11, 17])
+#
+# plt.show()
 
 import matplotlib.pyplot as plt
 
-figure = plt.figure()
-ax1 = figure.add_subplot(1, 2, 1)
-ax2 = figure.add_subplot(1, 2, 2)
+option_votes = [63, 28, 8]
+option_names = [
+    "Flask"
+    "Django"
+    "It depends"
+]
 
-ax1.plot([1, 2, 3, 4], [3, 5, 9, 25])
-ax2.plot([1, 2, 3, 4], [5, 7, 11, 17])
+figure = plt.figure()
+axes = figure.add_subplot()
+
+axes.pie(
+    option_votes,
+    labels=option_names,
+    explode=[0.1, 0, 0]
+)
 
 plt.show()
