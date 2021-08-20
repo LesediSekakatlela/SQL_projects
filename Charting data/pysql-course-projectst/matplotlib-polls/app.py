d2d205af0cb7f4f2cf1f511986a25c70f66807fa
Lesedi <leseditumelo32@gmail.com>
2021-08-20 09:30:42 +0200

    stacked bar

--- Charting data/pysql-course-projectst/matplotlib-polls/app.py
+++ Charting data/pysql-course-projectst/matplotlib-polls/app.py
@@ -1,6 +1,24 @@
 import matplotlib.pyplot as plt
-import charts
-import database
+from data import polls
 
-charts.create_bar_chart(database.get_polls_and_votes())
+poll_titles = [poll[0] for poll in polls]
+poll_men = [poll[1] for poll in polls]
+poll_women = [poll[2] for poll in polls]
+
+poll_x_coordinates = range(len(polls))
+
+figure = plt.figure(figsize=(6, 6))
+axes = figure.add_subplot()
+axes.bar(
+    poll_x_coordinates,
+    poll_men,
+    tick_label=poll_titles
+)
+axes.bar(
+    poll_x_coordinates,
+    poll_women,
+    tick_label=poll_titles,
+    bottom=poll_men
+)
+
 plt.show()
---
+++ Charting data/pysql-course-projectst/matplotlib-polls/data.py
@@ -0,0 +1,8 @@
+polls = [
+    ("Flask vs. Django", 60, 19),
+    ("Who will win the election?", 15, 36),
+    ("Golf vs. Cricket", 26, 40),
+    ("Mac vs. Windows", 25, 34),
+    ("What ia the most popular type of graph?", 20, 13),
+    ("Who is the podcasting king?", 11, 6),
+]
